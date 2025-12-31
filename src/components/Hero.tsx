ba522eb99770bfd38784b7722d1b638bfd94bf08
Ahmad Naween Samandar <asama017@uottawa.ca>
2025-12-30 21:52:21 -0500

    Feat: initial commit, relative libraries imported and interface created

---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+
+import { Upload, Sparkles } from "lucide-react";
+import { useState } from "react";
+
+
+//interface for second part of the main page
+//it control page navigation
+interface HeroProps {
+  onNavigate?: (page: string) => void;
+  onAuthSuccess?: () => void;
+}
+
+export function Hero({ onNavigate, onAuthSuccess }: HeroProps = {}) {
+
+
+    return (
+        //to be coded
+    )
+}
