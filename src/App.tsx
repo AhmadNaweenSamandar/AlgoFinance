f77f28dd4c2156a9c4c91f0d7f5082c79bf4980f
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-01-01 20:51:33 -0500

    Update App.tsx
    the App.css file is commented out form this branch's snippet to allow the build to run

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+// the App.css file is commented out form this branch's snippet to allow the build to run
 
 function App() {
   const [count, setCount] = useState(0)
