117b069a97836dc82134c8d310f48f78f3003f17
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-02-21 19:45:29 -0500

    CHANGE: 5136 - > 5173 for frontend local host

--- packages/server/app/main.py
+++ packages/server/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 # import router from endpoints module
 from src.api.endpoints import router as api_router
 
@@ -8,20 +9,20 @@ from src.api.endpoints import router as api_router
 app = FastAPI(title="AlgoFinance API")
 
 # --- CORS (Cross-Origin Resource Sharing) Configuration ---
-# It tells the server to allow requests 
+# It tells the server to allow requests
 # coming from a specific "origin" (in this case, the local frontend at port 5136).
 # Without this, the browser would block the frontend from talking to this API.
 origins = [
-    "http://localhost:5136",
-    "http://127.0.0.1:5136"
+    "http://localhost:5173",  # frontend local development URL
+    "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,    # Only allow requests from these specific URLs
-    allow_credentials=True,   # Allow cookies/authentication headers
-    allow_methods=["*"],      # Allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],      # Allow all headers
+    allow_origins=origins,  # Only allow requests from these specific URLs
+    allow_credentials=True,  # Allow cookies/authentication headers
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
 )
 
 
@@ -36,25 +37,20 @@ memory_db = {"fruits": []}
 # e.g., /api/upload-statement
 app.include_router(api_router, prefix="/api")
 
+
 # GET Endpoint: Used to retrieve data.
 @app.get("/")
 def health_check():
     """
-    A simple endpoint to verify the backend is running 
+    A simple endpoint to verify the backend is running
     and reachable from the frontend.
     """
     return {"status": "active", "message": "Backend is running!"}
 
-# we can run main.py with: uvicorn app.main:app --reload
 
+# we can run main.py with: uvicorn app.main:app --reload
 
 
 # # --- Server Execution ---
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
-
-
-
